11d0029822b35a0be758ff42b5e5a15c3838eff4
Robert_Norwood <robnorwood91@gmail.com>
2021-01-04 21:42:57 -0800

    Created a spinner component, to render a spinner when fetching data

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,27 +1,39 @@
-import React, { Component } from 'react'
+import React from 'react'
 import UserItem from './UserItem'
+import Spinner from '../layout/Spinner'
+import PropTypes from 'prop-types'
 
-export default class Users extends Component {
-
-
-    render() {
-
+//destructuring the props that are being passed in from the app.js file
+export default function Users({ users, loading }) {
+    if (loading) {
+        return <Spinner />
+    } else {
         //the props for "users" is being passed in to the User component (this one) from the app.js main file since we decided to set the state inside of that file. 
         //when you display a list, the child needs a key which in this case will be the user.id
 
         return (
             <div style={userStyle}>
-                {this.props.users.map(user => {
+                {users.map(user => {
                     return <UserItem key={user.id} user={user} />
                 })}
 
             </div>
         )
+
     }
 }
+
+
+Users.propTypes = {
+    users: PropTypes.array.isRequired,
+    loading: PropTypes.bool.isRequired
+}
+
 
 const userStyle = {
     display: 'grid',
     gridTemplateColumns: 'repeat(3, 1fr)',
     gridGap: '1rem'
 }
+
+
