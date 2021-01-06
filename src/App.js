47c361537f9b6a2273654b2756f7d3fd604b1e3a
Robert_Norwood <robnorwood91@gmail.com>
2021-01-05 23:53:26 -0800

    Created a single user component to show more info about a user

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component, Fragment } from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import Navbar from './components/layout/Navbar'
 import Users from './components/users/Users'
+import User from './components/users/User'
 import Search from './components/users/Search'
 import Alert from './components/layout/Alert'
 import About from './components/pages/About'
@@ -13,38 +14,31 @@ export default class App extends Component {
 
   state = {
     users: [],
+    user: {},
     loading: false,
     alert: null
   }
 
-  /*lifecycle method component did mount
-  //this will allow there to be users showing up on the page when you go to the site
-  async componentDidMount() {
-    this.setState({
-      //this is loading(set to true) while gathering the data from the get requests, once the data is received, it will be set back to false
-      loading: true
-    })
-    //get request to the url for the data
-    const res = await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_ID}`)
-
-    this.setState({
-      //set the users state to be the array of data we receive back from the get request
-      users: res.data,
-      //sets the loading state back to false
-      loading: false
-    })
-  }
-  */
-
   //Search Github users
-  searchUsers = async (text) => {
+  searchUsers = async text => {
 
     this.setState({ loading: true })
 
-    const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_ID}`)
+    const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`)
 
     this.setState({
       users: res.data.items,
+      loading: false
+    })
+  }
+
+  //Get single Github user
+  getUser = async username => {
+    this.setState({ loading: true })
+    const res = await axios.get(`https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`)
+
+    this.setState({
+      user: res.data,
       loading: false
     })
   }
@@ -56,19 +50,14 @@ export default class App extends Component {
       loading: false
     })
   }
-
-
+  //Alert 
   setAlert = (msg, type) => {
-    this.setState({
-      alert: {
-        msg, type
-      }
-    })
+    this.setState({ alert: { msg, type } })
     setTimeout(() => this.setState({ alert: null }), 3000)
   }
+
   render() {
-    const { users, loading } = this.state
-    //we are passing in loading and users from our state, as props to the Users component
+    const { users, user, loading } = this.state
 
     return (
       <BrowserRouter>
@@ -92,7 +81,15 @@ export default class App extends Component {
                 </Fragment>
               )} />
 
-              <Route exact path="/about" component={About} />
+              <Route
+                exact
+                path="/about"
+                component={About}
+              />
+              <Route exact path='/user/:login' render={props => (
+                <User {...props} getUser={this.getUser} user={user} loading={loading} />
+              )} />
+
             </Switch>
           </div>
 
--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,14 +1,9 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
 
-//another way to destructure the props is to pass it as an argument to the UserItem function like so
 export default function UserItem({ user: { login, avatar_url, html_url } }) {
 
-    //So, since we passed in props from the Users component, we can now destructure those props and pull out what we need. We are pulling the below three things from the user objects in the Users' component's state(represented by the "user" variable in the map function), and setting them to this.props.user. The "user" keyword at the end of this.props.user, has all of the user information inside of it (this is the same user variable from the map function), and we are just pulling the pieces of info that we need to use.
-
-    //const { login, avatar_url, html_url } = props.user;
-
-    //essentially the UserItem component is just displaying the information from the props that were passed in from the Users component.
     return (
         <div className="card text-center">
             <img
@@ -19,7 +14,7 @@ export default function UserItem({ user: { login, avatar_url, html_url } }) {
 
             <h3>{login}</h3>
             <div>
-                <a href={html_url} className="btn btn-dark btn-sm my-1">More</a>
+                <Link to={`/user/${login}`} className="btn btn-dark btn-sm my-1">More</Link>
             </div>
         </div>
     )
