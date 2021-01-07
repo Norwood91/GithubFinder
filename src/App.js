4633f0558ffb91666a0bc8c38b8cfa0a02afa3d3
Robert_Norwood <robnorwood91@gmail.com>
2021-01-07 01:47:45 -0800

    implementing context and reducer. Cleaning up app.js

--- src/App.js
+++ src/App.js
@@ -1,100 +1,99 @@
-import React, { useState, Fragment } from 'react'
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
-import Navbar from './components/layout/Navbar'
-import Users from './components/users/Users'
-import User from './components/users/User'
-import Search from './components/users/Search'
-import Alert from './components/layout/Alert'
-import About from './components/pages/About'
-import axios from 'axios'
+import React, { useState, Fragment } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import Users from './components/users/Users';
+import User from './components/users/User';
+import Search from './components/users/Search';
+import Alert from './components/layout/Alert';
+import About from './components/pages/About';
+import axios from 'axios';
+import GithubState from './context/github/GithubState';
+
 import './App.css';
 
+export default function App() {
+	const [users, setUsers] = useState([]);
+	const [user, setUser] = useState({});
+	const [repos, setRepos] = useState([]);
+	const [loading, setLoading] = useState(false);
+	const [alert, setAlert] = useState(null);
 
-export default function App(){
-  const [users, setUsers] = useState([])
-  const [user, setUser] = useState({})
-  const [repos, setRepos] = useState([])
-  const [loading, setLoading] = useState(false)
-  const [alert, setAlert] = useState(null)
+	//Get single Github user
+	const getUser = async (username) => {
+		setLoading(true);
+		const res = await axios.get(
+			`https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+		);
 
+		setUser(res.data);
+		setLoading(false);
+	};
 
-  //Search Github users
-  const searchUsers = async text => {
+	//Get user repos
+	const getUserRepos = async (username) => {
+		setLoading(true);
+		const res = await axios.get(
+			`https://api.github.com/users/${username}/repos?per_page=1&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+		);
 
-    setLoading(true)
+		setRepos(res.data);
+		setLoading(false);
+	};
 
-    const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`)
+	//Clear Users
+	const clearUsers = () => {
+		setUsers([]);
+		setLoading(false);
+	};
+	//Alert
+	const showAlert = (msg, type) => {
+		setAlert({ msg, type });
+		setTimeout(() => setAlert(null), 3000);
+	};
 
-    setUsers(res.data.items)
-    setLoading(false)
-  }
+	return (
+		<GithubState>
+			<BrowserRouter>
+				<div className='App'>
+					<Navbar />
+					<div className='container'>
+						<Alert alert={alert} />
+						<Switch>
+							<Route
+								exact
+								path='/'
+								render={(props) => (
+									<Fragment>
+										<Search
+											clearUsers={clearUsers}
+											showClearButton={users.length > 0 ? true : false}
+											setAlert={showAlert}
+										/>
 
-  //Get single Github user
-  const getUser = async username => {
-    setLoading(true)
-    const res = await axios.get(`https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`)
+										<Users loading={loading} users={users} />
+									</Fragment>
+								)}
+							/>
 
-    setUser(res.data)
-    setLoading(false)
-  }
-
-  //Get user repos
-  const getUserRepos = async username => {
-    setLoading(true)
-    const res = await axios.get(`https://api.github.com/users/${username}/repos?per_page=1&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`)
-
-    setRepos(res.data)
-    setLoading(false)
-    
-  }
-
-  //Clear Users
-  const clearUsers = () => {
-    setUsers([])
-    setLoading(false)
-  }
-  //Alert 
-  const showAlert = (msg, type) => {
-    setAlert({ msg, type })
-    setTimeout(() => setAlert(null), 3000)
-  }
-
-    return (
-      <BrowserRouter>
-        <div className='App'>
-          <Navbar />
-          <div className="container">
-            <Alert alert={alert} />
-            <Switch>
-              <Route exact path="/" render={props => (
-                <Fragment>
-                  <Search
-                    searchUsers={searchUsers}
-                    clearUsers={clearUsers}
-                    showClearButton={users.length > 0 ? true : false}
-                    setAlert={showAlert} />
-
-                  <Users
-                    loading={loading}
-                    users={users} />
-
-                </Fragment>
-              )} />
-
-              <Route
-                exact
-                path="/about"
-                component={About}
-              />
-              <Route exact path='/user/:login' render={props => (
-                <User {...props} getUser={getUser} user={user} loading={loading} getUserRepos={getUserRepos} repos={repos} />
-              )} />
-
-            </Switch>
-          </div>
-
-        </div>
-      </BrowserRouter>
-    );
+							<Route exact path='/about' component={About} />
+							<Route
+								exact
+								path='/user/:login'
+								render={(props) => (
+									<User
+										{...props}
+										getUser={getUser}
+										user={user}
+										loading={loading}
+										getUserRepos={getUserRepos}
+										repos={repos}
+									/>
+								)}
+							/>
+						</Switch>
+					</div>
+				</div>
+			</BrowserRouter>
+		</GithubState>
+	);
 }
-
--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,48 +1,46 @@
-import React, {useState} from 'react'
+import React, { useState, useContext } from 'react';
+import GithubContext from '../../context/github/githubContext';
 
+export default function Search({ showClearButton, clearUsers, setAlert }) {
+	const githubContext = useContext(GithubContext);
 
-export default function Search({searchUsers, showClearButton, clearUsers, setAlert}) {
-    const [text, setText] = useState('')
-   
-    const onSubmit = (e) => {
-        e.preventDefault()
-        if (text === "") {
-            setAlert('Please enter a name', 'danger')
-        } else {
-            searchUsers(text)
-            setText('')
-        }
+	const [text, setText] = useState('');
 
-    }
+	const onSubmit = (e) => {
+		e.preventDefault();
+		if (text === '') {
+			setAlert('Please enter a name', 'danger');
+		} else {
+			githubContext.searchUsers(text);
+			setText('');
+		}
+	};
 
-    const onChange = (e) => {
-        setText(e.target.value)
-    }
-        return (
-            <div>
-                <form onSubmit={onSubmit} className="form">
-                    <input
-                        type="text"
-                        name="text"
-                        placeholder="Search Users..."
-                        value={text}
-                        onChange={onChange}
-                    />
+	const onChange = (e) => {
+		setText(e.target.value);
+	};
+	return (
+		<div>
+			<form onSubmit={onSubmit} className='form'>
+				<input
+					type='text'
+					name='text'
+					placeholder='Search Users...'
+					value={text}
+					onChange={onChange}
+				/>
 
-                    <input
-                        type="submit"
-                        value="Search"
-                        className="btn btn-dark btn-block" />
-                </form>
-                {
-                    showClearButton
-                        ?
-                        <button className="btn btn-light btn-block" onClick={clearUsers}>Clear</button>
-                        :
-                        null
-                }
-            </div>
-
-    )
+				<input
+					type='submit'
+					value='Search'
+					className='btn btn-dark btn-block'
+				/>
+			</form>
+			{showClearButton ? (
+				<button className='btn btn-light btn-block' onClick={clearUsers}>
+					Clear
+				</button>
+			) : null}
+		</div>
+	);
 }
-
